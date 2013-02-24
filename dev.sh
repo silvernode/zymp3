5d04fdf49ce0a01f30d2c725a7a6c11eff312eac
silvernode <silvernode@gmail.com>
2013-02-24 02:09:18 -0600

    Removed dev.sh as it is not needed

--- dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-t=$(zenity --entry --title="test" --text="name the file: "); zenity --file-selection --save --text="$t" --confirm-overwrite
-
-#$t | 
-zenity --file-selection --save --confirm-overwrite
