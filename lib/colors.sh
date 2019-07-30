e2c98c100fe5e781ce542525ec351be64abcde7a
mollusk <silvernode@gmail.com>
2019-07-30 01:28:31 -0700

    Add color variables

---
+++ lib/colors.sh
@@ -0,0 +1,16 @@
+# Color Vars
+# Usage: echo -e "${LRED}This is light red text${NC}"
+
+RED='\033[0;31m'
+LRED="\033[1;31m"
+BLUE="\033[0;34m"
+LBLUE="\033[1;34m"
+GREEN="\033[0;32m"
+LGREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+CYAN="\033[0;36m"
+LCYAN="\033[1;36m"
+PURPLE="\033[0;35m"
+LPURPLE="\033[1;35m"
+BWHITE="\e[1m"
+NC='\033[0m' # No Color
